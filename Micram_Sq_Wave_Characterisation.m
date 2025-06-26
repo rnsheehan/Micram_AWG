63b2c960e875122d153584c5fed2a69cea4dba82
rnsheehan <robertnsheehan@gmail.com>
2025-06-26 12:38:52 +0100

    Micram Coding
    I've discovered an island of instability in the Micram. Output seems unstable when clock rate is between 25GHz and 29GHz. Stable output returns for 30GHz to 34GHz, didn't test above that range. Also added a script for examining purely square wave output observed the same effect. Measured the same pulse widths and pulse separations. Twin Pulse output is operating correctly.

--- Micram_Sq_Wave_Characterisation.m
+++ Micram_Sq_Wave_Characterisation.m
@@ -1,11 +1,11 @@
-% script for configuring the Micram DAC4 AWG with Odhran Liston Twin-Pulse
+% script for configuring the Micram DAC4 AWG with Square Wave Pulses
 % need this script because of comms issues with the HMC-T2XXX
 % HMC-T2XXX controls clock rate for AWG
 % clock rate value will have to be entered manually in this script for
 % output to be configured correctly
-% Micram_Control.m is much nicer because it talks to DAC4 and HMC-T2XXX
-% simultaneously
-% R. Sheehan 25 - 6 - 2025
+% Want to see what the pulse width is for purely square waves as function
+% of Clock frequency
+% R. Sheehan 26 - 6 - 2025
 
 % Unclear if I'm allowed to post Micram GmbH MATLAB code on github so I won't
 % Add the search path directly
@@ -16,9 +16,11 @@ addpath(genpath('c:/Users/Robert/Equipment/Equipment_Manuals/Micram/DAC4_1.5/'))
 global md;
 
 % some constants for the twin-pulse pattern
-Ni = 10; % no. symbols with which twin-pulse pattern is buffered
+Ntot = 256; % total number of symbols in a line
 Nw = 5; % no. symbols needed to form pulse of known deltaT
-Nd = 4*Nw; % no. symbols needed to form delay between pulses
+Nd = Nw; % no. symbols needed to form delay between pulses
+Ns = Nd + Nw; % total no. symbols in single square segment
+NNs = round(Ntot / Ns); % no. segments that can fit in a line
 
 % set sample rate and the module no. for Micram DAC4
 sample_rate = 100e9; % DAC SR in units of Hz
@@ -48,25 +50,22 @@ fprintf('Desired Clock Frequency: %0.3f GHz\n',crghz);
 % R. Sheehan 24 - 6 - 2025
 %Nd = Micram_Get_Twin_Pulse_Delay_Symbols(crghz, deltaTd); 
 fprintf('Number of symbols in the delay %d\n',Nd);
+fprintf('Number of symbols in the segments %d\n',Ns);
+fprintf('Number of segments in the line %d\n',NNs);
 
-% Compute the size of the buffer
-buf_2 = 256 - 2*Nw - Nd; 
-if mod(buf_2, 2) == 0
-    Ni = buf_2 / 2; % buf_2 is even
-else
-    Ni = (buf_2-1) / 2; % buf_2 is odd, change it to nearest even
-end
-fprintf('Number of symbols in the buffer %d\n',Ni);
-
-% assign the twin-pulse pattern
+% assign the square wave pulse pattern
 % pattern takes the form of a line of length 256 symbols, repeated 2048 times
-% line will take the form [ 0 for Ni symbols (buffer), 63 for Nw symbols (first pulse), 0 for Nd symbols (delay), 63 for Nw symbols (second pulse), 0 for Ni symbols (buffer), zero padding ]
+% line will take the form [ 0 for Nd symbols (delay), 63 for Nw symbols (pulse),...., zero padding ]
 % zero padding is added on at the end of the line by DAC4 to make sure the line length is 256 symbols
 % the contents of line can be changed as needed
-buffer = zeros(1, Ni); % fixed buffer of zeroes to go at start and end of line
 pulse = 63 * ones(1, Nw); % pulse of known duration and max output voltage
 delay = zeros(1, Nd); % delay of known duration
-line = [buffer pulse delay pulse buffer];
+segment = [delay pulse]; 
+line = []; 
+for i=1:NNs
+    line = horzcat(line, segment); 
+end
+fprintf('Number of symbols in the line %d\n',size(line));
 
 % set sample rate
 dac4_set_samplerate( board, md.(board).devs, sample_rate)
